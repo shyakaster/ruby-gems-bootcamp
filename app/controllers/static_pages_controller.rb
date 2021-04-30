28e4a60c01cb6bb4784f5d1d8fc669c206f556fd
alex shyaka <alex.nkusi@codeimpact.co>
2021-04-30 13:47:47 +0300

    Add stats on top of popular courses

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def landing_page
+    @courses = Course.all.limit(3)
+    @recent_courses = Course.all.limit(3).order(created_at: :desc)
   end
-
+  
   def privacy_policy
   end
 end
