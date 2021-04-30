e25909055cf557be9e7ebd43757ad598da6032b4
alex shyaka <alex.nkusi@codeimpact.co>
2021-04-30 14:37:43 +0300

    Add gem friendly_id

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,9 +2,22 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: {minimum: 5}
   belongs_to :user
-  
+
   def to_s
     title
   end
   has_rich_text :description
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  # friendly_id :generated_slug, use: :slugged
+  # def generated_slug
+  #   require 'securerandom'
+  #   @random_slug ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  # end
+  #
+  # def to_s
+  #   slug
+  # end
+
 end
