9685e8d636dcaffd3bed13b7e31c5d8b06aaee9e
alex shyaka <alex.nkusi@codeimpact.co>
2021-05-31 22:28:11 +0300

    add and configure rolify gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable,:confirmable
+  
+  rolify
 
  def to_s
    email
@@ -12,4 +14,17 @@ class User < ApplicationRecord
  def username
   self.email.split(/@/).first
  end
+
+ after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.roles.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student) if self.roles.blank?
+      self.add_role(:teacher) #if you want the user to be able to create own courses
+    end
+  end
 end
